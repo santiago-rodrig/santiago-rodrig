b73eb5ee0507aa1a67aa1dd4fe1fdacbb8147677
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-13 16:17:26 -0400

    feature: mac terminal like box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import 'particles.js';
+import Typewriter from 'typewriter-effect/dist/core';
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', './particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+const typeWriter = new Typewriter('#typewriter', {
+  wrapperClassName: 'type-wrapper',
+  cursorClassName: 'type-cursor',
+  loop: true,
+});
+
+typeWriter.typeString('Hello World!').start();
