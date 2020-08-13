33cfaf438e75b23bf18ba69fa6a0a6285dfadbf7
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-13 17:15:19 -0400

    fix: tweak writing speed

--- src/index.js
+++ src/index.js
@@ -10,18 +10,22 @@ const typeWriter = new Typewriter('#typewriter', {
   wrapperClassName: 'type-wrapper',
   cursorClassName: 'type-cursor',
   loop: true,
+  delay: 40,
 });
 
 typeWriter
   .pauseFor(2000)
   .typeString('Hi, my name is Santiago Rodriguez')
   .pauseFor(2500)
-  .deleteAll(50)
+  .deleteAll(40)
+  .pauseFor(2000)
   .typeString('I\'m a full-stack web developer that loves to create things')
-  .pauseFor(2500)
-  .deleteAll(50)
+  .pauseFor(2000)
+  .deleteAll(40)
+  .pauseFor(2000)
   .typeString('I am a creator, an innovator, and a great team player')
-  .pauseFor(2500)
-  .deleteAll(50)
+  .pauseFor(2000)
+  .deleteAll(40)
+  .pauseFor(2000)
   .start()
 
