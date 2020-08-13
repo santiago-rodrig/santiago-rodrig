3440f4b1b22463e47d0c9b7be833951161ef7ca8
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-13 16:25:24 -0400

    feature: intro about myself

--- src/index.js
+++ src/index.js
@@ -12,4 +12,15 @@ const typeWriter = new Typewriter('#typewriter', {
   loop: true,
 });
 
-typeWriter.typeString('Hello World!').start();
+typeWriter
+  .typeString('Hi, my name is Santiago Rodriguez')
+  .pauseFor(2500)
+  .deleteAll(50)
+  .typeString('I\'m a full-stack web developer that loves to create things')
+  .pauseFor(2500)
+  .deleteAll(50)
+  .typeString('I am a creator, an innovator, and a great team player')
+  .pauseFor(2500)
+  .deleteAll(50)
+  .start()
+
